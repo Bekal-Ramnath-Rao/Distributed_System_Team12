89da45ac9adc9b5911262a018ac78c26c70c70f4
Bekal-Ramnath-Rao <ramnathraobekal@gmail.com>
2025-01-03 13:39:20 +0100

    stackoverflow code

--- server/multicast_sender.py
+++ server/multicast_sender.py
@@ -1,18 +1,16 @@
-# SENDER
-
 import socket
 
-group = '224.1.1.1'
-port = 5004
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+# regarding socket.IP_MULTICAST_TTL
+# ---------------------------------
+# for all packets sent, after two hops on the network the packet will not 
+# be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+MULTICAST_TTL = 2
 
-# 2-hop restriction in network
-ttl = 2
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
-sock = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM,
-                     socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP,
-                socket.IP_MULTICAST_TTL,
-                ttl)
-
-sock.sendto(b"hello world", (group, port))
+# For Python 3, change next line to 'sock.sendto(b"robot", ...' to avoid the
+# "bytes-like object is required" msg (https://stackoverflow.com/a/42612820)
+sock.sendto("robot", (MCAST_GRP, MCAST_PORT))
