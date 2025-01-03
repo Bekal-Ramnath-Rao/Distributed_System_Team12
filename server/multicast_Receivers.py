63df266ca2bad767a538eeb2625bdd80b7cc6ad2
nandandevinagara <nandanparkala@gmail.com>
2025-01-03 13:31:46 +0100

    multicast dummy code is updated

--- server/multicast_Receivers.py
+++ server/multicast_Receivers.py
@@ -1,29 +1,23 @@
+# Multicast receiver
+# Guidance:  https://stackoverflow.com/a/1794373
 import socket
 import struct
-import sys
 
-multicast_group = "224.3.29.71"
-server_address = ("", 10000)
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+IS_ALL_GROUPS = True
 
-# Create the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if IS_ALL_GROUPS:
+  # on this port, receives ALL multicast groups
+  sock.bind(('', MCAST_PORT))
+else:
+  # on this port, listen ONLY to MCAST_GRP
+  sock.bind((MCAST_GRP, MCAST_PORT))
+mreq = struct.pack('4sl', socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
-# Bind to the server address
-sock.bind(server_address)
-
-# Tell the operating system to add the socket to the multicast group
-# on all interfaces.
-group = socket.inet_aton(multicast_group)
-mreq = struct.pack("4sL", group, socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-# Receive/respond loop
 while True:
-    print( "\nwaiting to receive message")
-    data, address = sock.recvfrom(1024)
-
-    print  ("received %s bytes from %s" % (len(data), address))
-    print ( data)
-
-    print  ("sending acknowledgement to", address)
-    sock.sendto("ack", address)
+  print(sock.recv(10240))
--- server/multicast_sender.py
+++ server/multicast_sender.py
@@ -1,42 +1,18 @@
+# Multicast sender
+# Guidance:  https://stackoverflow.com/a/1794373
 import socket
-import struct
-import sys
 
-message = "very important data"
-multicast_group = ("224.3.29.71", 10000)
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+MESSAGE = b'Hello, Multicast!'
 
-# Create the datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# regarding socket.IP_MULTICAST_TTL
+# ---------------------------------
+# for all packets sent, after two hops on the network the packet will not
+# be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+MULTICAST_TTL = 2
 
-# Set a timeout so the socket does not block indefinitely when trying
-# to receive data.
-sock.settimeout(10)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
-# Set the time-to-live for messages to 1 so they do not go past the
-# local network segment.
-ttl = struct.pack("b", 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-try:
-
-    # Send data to the multicast group
-    print( 'sending "%s"' % message)
-    sent = sock.sendto(message.encode(), multicast_group)
-
-    # Look for responses from all recipients
-    while True:
-        print( "waiting to receive")
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print("timed out, no more responses")
-            break
-        else:
-            print('received "%s" from %s' % (data, server))
-
-finally:
-    print( "closing socket")
-    sock.close()
-
-# if __name__ == "__main__":
-#     main()
+sock.sendto(MESSAGE, (MCAST_GRP, MCAST_PORT))
