0c639f2c04e28f3d6fffba31fbdefa2fc805b00f
nandandevinagara <nandanparkala@gmail.com>
2025-01-03 13:35:51 +0100

    new multicast code

--- server/multicast_sender.py
+++ server/multicast_sender.py
@@ -1,18 +1,18 @@
-# Multicast sender
-# Guidance:  https://stackoverflow.com/a/1794373
+# SENDER
+
 import socket
 
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
-MESSAGE = b'Hello, Multicast!'
+group = '224.1.1.1'
+port = 5004
 
-# regarding socket.IP_MULTICAST_TTL
-# ---------------------------------
-# for all packets sent, after two hops on the network the packet will not
-# be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
-MULTICAST_TTL = 2
+# 2-hop restriction in network
+ttl = 2
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+sock = socket.socket(socket.AF_INET,
+                     socket.SOCK_DGRAM,
+                     socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP,
+                socket.IP_MULTICAST_TTL,
+                ttl)
 
-sock.sendto(MESSAGE, (MCAST_GRP, MCAST_PORT))
+sock.sendto(b"hello world", (group, port))
